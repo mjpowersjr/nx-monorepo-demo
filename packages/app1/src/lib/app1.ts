ec58dc1b143bb098eab6a3d5d3583ed3ef2623a1
Mike Powers <mjpowersjr@gmail.com>
2025-05-01 08:04:14 -0400

    changes

--- packages/app1/src/lib/app1.ts
+++ packages/app1/src/lib/app1.ts
@@ -1,3 +1,6 @@
+import { pkg1 } from '@my-org/pkg1';
+
 export function app1(): string {
+  pkg1();
   return 'app1';
 }
--- packages/pkg1/src/lib/pkg1.spec.ts
+++ packages/pkg1/src/lib/pkg1.spec.ts
@@ -2,6 +2,6 @@ import { pkg1 } from './pkg1.js';
 
 describe('pkg1', () => {
   it('should work', () => {
-    expect(pkg1()).toEqual('pkg1');
+    expect(pkg1()).toMatch(/pkg.*/);
   });
 });
